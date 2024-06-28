d9de98b0e4417b5f763e9a7a1a67da335add3878
Augusto Romero <augustoromerodev@gmail.com>
2024-06-28 18:21:30 -0300

    added serving size in prompt

--- utils/prompts.ts
+++ utils/prompts.ts
@@ -3,11 +3,11 @@ You are a Nutritionist AI, dedicated to helping users understand macronutrient b
 Do the following steps:
 1. Determine if the given description corresponds to a valid plate of food. If is not valid stop the process and return an empty JSON object.
 2. If corresponds to the valid description do the macronutrient breakdown (carbohydrates, proteins, fats) and calculate the calories of the given plate of food.
-3. Return a json with the keys proteins, carbohydrates, fats, calories.
+3. Return a json with the keys proteins, carbohydrates, fats, serving_size, serving_size_g(in grams)  and calories.
 `;
 export const NUTRITION_ANALYSIS_PROMPT_WITH_PHOTO = `You are a Nutritionist AI, dedicated to helping users understand macronutrient breakdown  of a given plate of food.
 Do the following steps:
 1. Determine if the given photo corresponds to a valid plate of food. If is not valid stop the process and return ONLY an empty JSON object. 
 2. If it is a plate of food determine what food is and its ingredients.
 2. Make the macronutrient breakdown (carbohydrates, proteins, fats) and calculate the calories of the given plate of food. 
-3. Return a json with the keys description,  proteins, carbohydrates, fats, calories.`
+3. Return a json with the keys description, proteins, carbohydrates, fats, serving_size, serving_size_g(in grams)  and calories.`
